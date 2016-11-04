c54d9758536621fa6c8ec4ef772b3118626a7264
Zachary Schreiber <zachgives@gmail.com>
2016-11-03 21:06:14 -0400

    Started Angular stuff :(

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,24 @@
 
-var SpielApp = angular.module('SpielApp', []);
+ angular.module('spielapp', ['ui.router', 'LocalStorageModule'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/index.html');
+
+  $stateProvider.state('main', {
+    url: '/index.html',
+    abstract: true,
+    template: '<ui-view></ui-view>'
+  }).state('main.login', {
+    url: 'login',
+    templateUrl: './src/templates/login.html',
+    controller: 'LoginController'
+  }).state('main.game', {
+    url: 'game',
+    templateUrl: './src/templates/game.html',
+    controller: 'MainController'
+  }).state('main.favs', {
+    url: 'favs',
+    templateUrl: './src/templates/favs.html',
+    controller: 'FavController'
+  });
+});
--- src/js/controllers/MainController.js
+++ src/js/controllers/MainController.js
@@ -8,4 +8,44 @@ SpielApp.controller('MainController', function(spielForm, $scope) {
       });
 
 
+function libMaker(string) {
+    var split = string.split(" ");
+
+     for (var i = 0; i < split.length; i++) {
+       switch(split[i]) {
+         case "1":
+         split[i] = "<input type='text' class='form noun' placeholder='noun required >";
+         break;
+         case "2":
+         split[i] = "<input type='text' class='form verb' placeholder='verb' required>";
+         break;
+         case "3":
+         split[i] = "<input type='text' class='form adjective' placeholder='adjective' required>";
+         break;
+         case "4":
+         split[i] = "<input type='text' class='form pluralnoun' placeholder='plural noun'>";
+         break;
+         case "5":
+         split[i] = "<input type='text' class='form animal' placeholder='animal'>";
+         break;
+         case "6":
+         split[i] = "<input type='text' class='form pluralanimal' placeholder='plural animal'>";
+         break;
+         case "7":
+         split[i] = "<input type='text' class='form nationality' placeholder='nationality'>";
+         break;
+
+       }
+     }
+     console.log(split.join(' '));
+   }
+
+
+
+ var string= "Waiter Thank you for dining at 1  . 1 grille. Tonights specials are To start we have 2. jam with toasted 4 stuff things people's 2. dlaf ofe, dfof 7.";
+
+
+
+
+
   }]);
