9836e53d2732f7ac2651f1dc973d1bb04fd7952b
Zachary Schreiber <zachgives@gmail.com>
2016-11-05 19:23:31 -0400

    nothing accomplished

--- src/js/controllers/MainController.js
+++ src/js/controllers/MainController.js
@@ -9,12 +9,14 @@ app.controller('MainController', function($scope, $sce) {
       $scope.badInput = true;
     };
 
-
-
+    $scope.doBlur = function(){
+      document.getElementsByClassName('gameForm').attr('placeholder') == this.val();
+    };
 
 
 
     $scope.numString = "Pizza was invented by a  7 chef. To make a pizza, you need to take a lump of 1 , and make a thin, round 3 1 . Then you cover it with 3 sauce, 3 cheese, and fresh chopped 4 . Next you have to bake it in a very hot 1 . When it is done, 2 it into 3 slices.";
+
     $scope.body = null;
     $scope.completedSpiel = null;
 
@@ -22,14 +24,14 @@ app.controller('MainController', function($scope, $sce) {
     $scope.textarea={
   field1:''
 }
-  console.log($scope.textarea.field1);
+
 $scope.libMaker= function() {
 
     this.splitUp = $scope.numString.split(" ");
      for (var i = 0; i < this.splitUp.length; i++) {
        switch(this.splitUp[i]) {
          case "1":
-         this.splitUp[i] = "<input type='text' class='gameForm' placeholder='NOUN' required >";
+         this.splitUp[i] = "<input type='text' class='gameForm' placeholder='NOUN'  required >";
          break;
          case "2":
          this.splitUp[i] = "<input type='text' class='gameForm' placeholder='VERB' required>";
@@ -62,11 +64,12 @@ $scope.libMaker= function() {
 
   });
 
-  app.filter('html', function($sce) {
-    return function(val) {
-        return $sce.trustAsHtml(val);
-    };
-});
+
+//   app.filter('html', function($sce) {
+//     return function(val) {
+//         return $sce.trustAsHtml(val);
+//     };
+// });
 
 //
 //  var string= "Waiter Thank you for dining at 1  . 1 grille. Tonights specials are To start we have 2. jam with toasted 4 stuff things people's 2. dlaf ofe, dfof 7.";
