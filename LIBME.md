563867bcbe3878812208b49ec3c5e1eb1da752f8
Zachary Schreiber <zachgives@gmail.com>
2016-11-06 13:40:30 -0500

    Much Changes.  Added jQuery, and game instructions

--- LIBME.md
+++ LIBME.md
@@ -1,19 +1,21 @@
 
-  Waiter: Thank you for dining at ADJECTIVE NOUN grille. Tonight's specials are:
-  To start, we have NOUN jam with toasted PLURAL-NOUN.
-  Our salad is PLURAL-NOUN tossed with sprouted PLURAL-NOUN.
-  Our entree is free-range NOUN and an ADJECTIVE NOUN pan sauce.
-  To drink, we have a Singapore VERB. 
+  Waiter: Thank you for dining at 3 1 grille. Tonight's specials are:
+   1 jam with toasted 4 .
+  Our salad is 4 tossed with sprouted 4 .
+  Our entree is free-range 1 and an 3 1 pan sauce.
+  To drink, we have a Singapore 2 .
+
+
+  Come 2 at WALMART, where you`ll receive 3 discounts on all of your favorite brand name 4 . Our 3 and 3 associates are there to 2 you. We have 1 for the moms, 1 for the kids and all the latest electronics for the 6 . So come on down to your 3  WALMART where the 4 come first.
+
+  Is your 1 making a mess of your 1 ? Try 1 ! It will 2 your whole 1 and 2 to unbelievable levels! Watch, as we demonstrate, the 3 capabilities of this 3  product! Everyone should have at least 8 of these! To order, call the number on your screen. Call within the next 8 minutes, and you will receive a 1 , 6 , and a 7 1 as a bonus! Call now!
+
+  Captain: Ohhhhh... who lives in a 1 under the 1 ? Kids: 2 Bob Squarepants! Captain: 3 and 9 and 3 is he! Kids: 1 Bob Squarepants! Captain: If 3 1 be somthing you 2 ... Kids: 2 Bob Squarepants! Captain: Then 2 on the 1 and 2 like a 5 . Kids: 1  Bob Squarepants! Captain: Ready? Everyone: 2  Bob Squarepants! 1 Bob Squarepants! 3 Bob Squarepants! Captain: 3 Bob... Squarepants! ahhhahhhahahhaha!
 
 
 
 
-NOUN panini with seasonal PLURAL-NOUN & ADJECTIVE      9
-pressed NOUN juice    8
-NATIONALITY NOUN with ADJECTIVE farm's NOUN cheese   11
-artisanal PLURAL-NOUN with rustic NOUN pudding     17
-natural ANIMAL extract    13
-braised NOUN shoulder 14
-kale with ADJECTIVE ANIMAL butter 9
-NATIONALITY NOUN tacos 7
-locally-sourced PLURAL-NOUN over burnt NOUN grits 13
+
+
+
+noun 1, verb 2, adjective 3, pluralnoun 4, animal 5, plural animal 6, nationality 7 number 8 color 9
--- src/js/controllers/MainController.js
+++ src/js/controllers/MainController.js
@@ -1,22 +1,18 @@
 var app = angular.module('spielapp')
 app.controller('MainController', function($scope, $sce) {
 
-    $scope.badInput = false;
+    
+
   	$scope.isActive = false;
-
-    $scope.activeForm = function(info) {
+    $scope.activeForm = function() {
       $scope.isActive = !$scope.isActive;
-      $scope.badInput = true;
-    };
-
-    $scope.doBlur = function(){
-      document.getElementsByClassName('gameForm').attr('placeholder') == this.val();
     };
 
 
 
-    $scope.numString = "Pizza was invented by a  7 chef. To make a pizza, you need to take a lump of 1 , and make a thin, round 3 1 . Then you cover it with 3 sauce, 3 cheese, and fresh chopped 4 . Next you have to bake it in a very hot 1 . When it is done, 2 it into 3 slices.";
 
+
+    $scope.numString = "Captain: Ohhhhh... who lives in a 1 under the 1 ? Kids: 1 Bob Squarepants! Captain: 3 and 9 and 3 is he! Kids: 1 Bob Squarepants! Captain: If 3 1 be somthing you 2 ... Kids: 1 Bob Squarepants! Captain: Then 2 on the 1 and 2 like a 5 . Kids: 1  Bob Squarepants! Captain: Ready? Everyone: 1  Bob Squarepants! 1 Bob Squarepants! 1 Bob Squarepants! Captain: 1 Bob... Squarepants! ahhhahhhahahhaha!";
     $scope.body = null;
     $scope.completedSpiel = null;
 
@@ -26,30 +22,35 @@ app.controller('MainController', function($scope, $sce) {
 }
 
 $scope.libMaker= function() {
-
     this.splitUp = $scope.numString.split(" ");
      for (var i = 0; i < this.splitUp.length; i++) {
        switch(this.splitUp[i]) {
          case "1":
-         this.splitUp[i] = "<input type='text' class='gameForm' placeholder='NOUN'  required >";
+         this.splitUp[i] = "<p contenteditable='true' class='gameForm'>NOUN</p>";
          break;
          case "2":
-         this.splitUp[i] = "<input type='text' class='gameForm' placeholder='VERB' required>";
+         this.splitUp[i] = "<p contenteditable='true' class='gameForm'>VERB</p>";
          break;
          case "3":
-         this.splitUp[i] = "<input type='text' class='gameForm' placeholder='ADJECTIVE' required>";
+         this.splitUp[i] = "<p contenteditable='true' class='gameForm'>ADJECTIVE</p>";
          break;
          case "4":
-         this.splitUp[i] = "<input type='text' class='gameForm'  placeholder='PLURAL NOUN'>";
+         this.splitUp[i] = "<p contenteditable='true' class='gameForm'>PLURAL-NOUN</p>";
          break;
          case "5":
-         this.splitUp[i] = "<input type='text' class='gameForm' placeholder='ANIMAL'>";
+         this.splitUp[i] = "<p contenteditable='true' class='gameForm'>ANIMAL</p>";
          break;
          case "6":
-         this.splitUp[i] = "<input type='text' class='gameForm'  placeholder='PLURAL ANIMAL'>";
+         this.splitUp[i] = "<p contenteditable='true' class='gameForm'>PLURAL ANIMAL</p>";
          break;
          case "7":
-         this.splitUp[i] = "<input type='text' class='gameForm' placeholder='NATIONALITY'>";
+         this.splitUp[i] = "<p contenteditable='true' class='gameForm'>NATIONALITY</p>";
+         break;
+         case "8":
+         this.splitUp[i] = "<p contenteditable='true' class='gameForm'>NUMBER</p>";
+         break;
+         case "9":
+         this.splitUp[i] = "<p contenteditable='true' class='gameForm'>COLOR</p>";
          break;
 
        }
